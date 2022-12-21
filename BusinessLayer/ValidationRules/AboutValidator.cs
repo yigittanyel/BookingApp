a9e28fca0ed76ea8a58b0d0b17eec95e99cb6a8a
yigittanyel <tanyelyigit@gmail.com>
2022-12-21 14:08:41 +0300

    first commit

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama kısmı boş geçilemez!");
+            RuleFor(x => x.Title).MinimumLength(4).WithMessage("Başlık en az 4 karakter olmalıdır.").MaximumLength(100).WithMessage("Başlık en fazla 100 karakter olabilir");
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,40 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        public Context()
+        {
+
+        }
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=STUS22\\SQL2019;Initial Catalog=fw_booking;Integrated Security=True;TrustServerCertificate=True");
+            }
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<About2> Abouts2 { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Feature2> Features2 { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
